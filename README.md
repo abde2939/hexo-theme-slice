734e9a51ceed904439f41fdab59c42b0fe6621db
NHibiki <i@nekoyu.cc>
2017-08-22 16:54:13 +1200

    init

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# hexo_theme_slice
-A simple/light/beautiful/onepaged Hexo Theme.
+# Slice
+A Simple, Light, Beautiful, Onepaged Hexo Theme.
+
+## Installation
+
+### Download
+
+```bash
+  git clone https://github.com/NHibiki/hexo_theme_slice.git theme/slice
+```
+
+Or, download the zip package through [GitHub](https://github.com/NHibiki/hexo_theme_slice/archive/master.zip).
+
+Then unzip the file to folder `theme/slice`
+
+### Config
+
+You need to install these plugins to enable the theme:
+
+ - hexo-generator-json-content
+ - hexo-generator-feed
+
+After that, you need to add some configs to Hexo Config.
+
+```yml
+theme: slice # To Enable The Theme.
+
+jsonContent:
+  meta: true
+  dateFormat: YYYY-MM-DD HH:mm:ss
+  posts:
+    title: true
+    slug: true
+    date: true
+    updated: true
+    comments: true
+    path: true
+    link: true
+    permalink: true
+    excerpt: true
+    keywords: true
+    text: true
+    raw: false
+    content: true
+    categories: true
+    tags: true
+```
+
+<span style="color:red">Make Sure "content" and "text" are set to true if you don't want any bother.</span>
+
+### Customize
+
+Then, it is time to work on the theme `_config.yml` file.
+
+First, rename `_config.yml.edit` which is a default config to `_config.yml`
+
---
+++ source/script/main.js
@@ -0,0 +1,205 @@
+var isLoaded = false;
+var CONTENT = {};
+var POSTS = [];
+var CATEGORIES = {};
+var TAGS = {};
+var CURRENT = -1;
+
+$(document).ready(function() {
+    var keepHash = null;
+    if (getHash() == "" || getHash() == "#") gotoHash("/home");
+    else keepHash = getHash().substring(1);
+    loadContents(() => {
+        if (keepHash) {
+            gotoHash(keepHash);
+        }
+    });
+    prepareSong();
+});
+
+var loadContents = (callback) => {
+    if (CONTENTURL == "" || isLoaded) {
+        callback();
+        return;
+    }
+    $.get(CONTENTURL, (e) => {
+        if (e) {
+            isLoaded = true;
+            CONTENT = e;
+            initContent();
+            showArticle();
+            loadArticleByNum(0);
+        }
+        callback();
+    });
+}
+
+var getIndexBySlug = (e) => {
+    for (var i = 0; i < POSTS.length; i++) {
+        if (POSTS[i].slug == e) return i;
+    }
+    return -1;
+}
+
+var initContent = () => {
+    POSTS = CONTENT.posts;
+    for (var i in POSTS) {
+        var _t = POSTS[i].tags;
+        for (var j in _t) {
+            if (TAGS.hasOwnProperty(_t[j].name)) {
+                TAGS[_t[j].name].push(i);
+            } else {
+                TAGS[_t[j].name] = [i];
+            }
+        }
+        var _c = POSTS[i].categories;
+        for (var j in _c) {
+            if (CATEGORIES.hasOwnProperty(_c[j].name)) {
+                CATEGORIES[_c[j].name].push(i);
+            } else {
+                CATEGORIES[_c[j].name] = [i];
+            }
+        }
+    }
+}
+
+var showMenu = (e = 1) => {
+    if (e != 1) {
+        if (e == 0) {
+            $("#menu").removeClass("show");
+            $("#open").removeClass("opened");
+        } else {
+            $("#menu").addClass("show");
+            $("#open").addClass("opened");
+        }
+        return;
+    }
+    if ($("#menu").hasClass("show")) {
+        $("#menu").removeClass("show");
+        $("#open").removeClass("opened");
+    } else {
+        $("#menu").addClass("show");
+        $("#open").addClass("opened");
+    }
+}
+
+var addTable = (a, b, c) => {
+    var _t = $("#linktable");
+    var html = _t.html();
+    
+    html += `<a href="${a}"><li>${b}</li><p>${c}</p></a>`;
+    _t.html(html);
+}
+
+var clearTable = () => {
+    var _t = $("#linktable");
+    _t.html("");
+}
+
+var showHome = () => {
+    loadArticleByNum(0);
+}
+
+var showLink = () => {
+    clearTable();
+    for (var i in FRIENDLIST) {
+        var name = i;
+        if (FRIENDLIST[i].indexOf("https://") == 0) {
+            name = "<green>[SSL]</green> " + i;
+        }
+        addTable(`${FRIENDLIST[i]}`, name, FRIENDLIST[i]);
+    }
+}
+
+var showCati = (e = null) => {
+    clearTable();
+    if (!e) {
+        for (var i in CATEGORIES) {
+            addTable(`#/category/${i}`, i, CATEGORIES[i].length);
+        }
+    } else {
+        if (CATEGORIES.hasOwnProperty(e)) {
+            var _p = CATEGORIES[e];
+            for (var i in _p) {
+                addTable(`#/article/${POSTS[_p[i]].slug}`, POSTS[_p[i]].title, POSTS[_p[i]].excerpt || POSTS[_p[i]].title);
+            }
+        } else {
+            gotoHash("/category");
+        }
+    }
+}
+
+var showTag = (e = null) => {
+    clearTable();
+    if (!e) {
+        for (var i in TAGS) {
+            addTable(`#/tag/${i}`, i, TAGS[i].length);
+        }
+    } else {
+        if (TAGS.hasOwnProperty(e)) {
+            var _p = TAGS[e];
+            for (var i in _p) {
+                addTable(`#/article/${POSTS[_p[i]].slug}`, POSTS[_p[i]].title, POSTS[_p[i]].excerpt || POSTS[_p[i]].title);
+            }
+        } else {
+            gotoHash("/tag");
+        }
+    }
+}
+
+var showArticle = () => {
+    clearTable();
+    for (var i = 0; i < POSTS.length; i++) {
+        addTable(`#/article/${POSTS[i].slug}`, POSTS[i].title, POSTS[i].excerpt || POSTS[i].title);
+    }
+}
+
+var loadArticleByName = (e) => {
+    loadArticleByNum(getIndexBySlug(e));
+}
+
+var loadArticleByNum = (e) => {
+    if (e >= 0 && e < POSTS.length) {
+        var _p = POSTS[e];
+        if (window.location.hash != "#/article/" + _p.slug) {
+            gotoHash("/article/" + _p.slug);
+        }
+        $("#atitle").html(_p.title);
+        $("#atime").html(_p.date);
+        buildList("#acate", "#/category/", _p.categories);
+        buildList("#atag", "#/tag/", _p.tags);
+        $("#acontent").html(_p.content);
+        CURRENT = e;
+        $('code').each(function(i, block) {hljs.highlightBlock(block);});
+        toTop();
+    }
+    showMenu(0);
+}
+
+var buildList = (a, b, c) => {
+    var element = $(a);
+    var html = "";
+    for (var i in c) {
+        html += `<a href="${b}${c[i].name}"><li class="fa">${c[i].name}</li></a>`;
+    }
+    element.html(html);
+}
+
+var next = () => {
+    loadArticleByNum(CURRENT + 1);
+}
+
+var prev = () => {
+    loadArticleByNum(CURRENT - 1);
+}
+
+var toTop = () => {
+    window.scrollTo(0, 0);
+}
+
+var prepareSong = () => {
+    nplayerList = SONGLIST;
+    if (nplayerList.length) {
+        playMusic(nplayerList[currentPlay]);
+    }
+}
